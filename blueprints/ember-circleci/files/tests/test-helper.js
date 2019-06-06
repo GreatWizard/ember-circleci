4b5e2d41abdb9a6858ccbcaefdff974b1d0936dc
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-06-06 12:41:01 +0200

    fix: test-helper by checking exam option (#19)

--- blueprints/ember-circleci/files/tests/test-helper.js
+++ blueprints/ember-circleci/files/tests/test-helper.js
@@ -1,8 +1,9 @@
 import Application from "../app";
 import config from "../config/environment";
 import { setApplication } from "@ember/test-helpers";
-import start from "ember-exam/test-support/start";
-
+<% if (exam) { %>import start from "ember-exam/test-support/start";
+<% } else { %>import { start } from "ember-qunit";
+<% } %>
 setApplication(Application.create(config.APP));
 
 start();
