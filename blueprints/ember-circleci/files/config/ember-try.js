aa104312cee604c66b1e4cb66b69243abf9311bd
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-07-05 11:11:26 +0200

    chore: use yarn command when possible

--- blueprints/ember-circleci/files/config/ember-try.js
+++ blueprints/ember-circleci/files/config/ember-try.js
@@ -1,79 +1,79 @@
-"use strict";
+'use strict';
 
-const getChannelURL = require("ember-source-channel-url");
+const getChannelURL = require('ember-source-channel-url');
 
 module.exports = function() {
   return Promise.all([
-    getChannelURL("release"),
-    getChannelURL("beta"),
-    getChannelURL("canary")
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary')
   ]).then(urls => {
     return {
 <% if (exam) { %>      command: `ember exam --split=${process.env.CIRCLE_NODE_TOTAL} --partition=${Number(process.env.CIRCLE_NODE_INDEX) + 1} --parallel --load-balance`,
 <% } if (yarn) { %>      useYarn: true,
 <% } %>      scenarios: [
         {
-          name: "ember-lts-2.18",
+          name: 'ember-lts-2.18',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              "jquery-integration": true
+              'jquery-integration': true
             })
           },
           npm: {
             devDependencies: {
-              "@ember/jquery": "^0.5.1",
-              "ember-source": "~2.18.0"
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~2.18.0'
             }
           }
         },
         {
-          name: "ember-lts-3.4",
+          name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
-              "ember-source": "~3.4.0"
+              'ember-source': '~3.4.0'
             }
           }
         },
         {
-          name: "ember-release",
+          name: 'ember-release',
           npm: {
             devDependencies: {
-              "ember-source": urls[0]
+              'ember-source': urls[0]
             }
           }
         },
         {
-          name: "ember-beta",
+          name: 'ember-beta',
           npm: {
             devDependencies: {
-              "ember-source": urls[1]
+              'ember-source': urls[1]
             }
           }
         },
         {
-          name: "ember-canary",
+          name: 'ember-canary',
           npm: {
             devDependencies: {
-              "ember-source": urls[2]
+              'ember-source': urls[2]
             }
           }
         },
         {
-          name: "ember-default",
+          name: 'ember-default',
           npm: {
             devDependencies: {}
           }
         },
         {
-          name: "ember-default-with-jquery",
+          name: 'ember-default-with-jquery',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              "jquery-integration": true
+              'jquery-integration': true
             })
           },
           npm: {
             devDependencies: {
-              "@ember/jquery": "^0.5.1"
+              '@ember/jquery': '^0.5.1'
             }
           }
         }
--- blueprints/ember-circleci/files/tests/test-helper.js
+++ blueprints/ember-circleci/files/tests/test-helper.js
@@ -1,8 +1,8 @@
-import Application from "../app";
-import config from "../config/environment";
-import { setApplication } from "@ember/test-helpers";
-<% if (exam) { %>import start from "ember-exam/test-support/start";
-<% } else { %>import { start } from "ember-qunit";
+import Application from '../app';
+import config from '../config/environment';
+import { setApplication } from '@ember/test-helpers';
+<% if (exam) { %>import start from 'ember-exam/test-support/start';
+<% } else { %>import { start } from 'ember-qunit';
 <% } %>
 setApplication(Application.create(config.APP));
 
