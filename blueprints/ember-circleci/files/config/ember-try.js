65fe6df3a512777a2e53288dcdce104c8354aecb
Guillaume Gérard <guillaume.gerard88@gmail.com>
2020-04-30 13:38:20 +0200

    feat: setup scenario parameter for test job

--- blueprints/ember-circleci/files/config/ember-try.js
+++ blueprints/ember-circleci/files/config/ember-try.js
@@ -5,9 +5,10 @@ const isCI = !!process.env.CI;
 
 module.exports = async function() {
   return {
-<% if (exam) { %>    command: `ember exam --split=${process.env.CIRCLE_NODE_TOTAL} --partition=${Number(process.env.CIRCLE_NODE_INDEX) + 1} --parallel --load-balance`,
-<% } else { %>    command: isCI ? 'ember test --silent --reporter xunit > ./reports/test_results.xml' : 'ember tests',
-<% } if (yarn) { %>    useYarn: true,
+<% if (exam) { %>    command: isCI ? `ember exam --split=${process.env.CIRCLE_NODE_TOTAL} --partition=${Number(process.env.CIRCLE_NODE_INDEX) + 1} --parallel --load-balance` : 'ember exam',
+<% } else { %>    command: isCI ? 'ember test --silent --reporter xunit > ./reports/test_results.xml' : 'ember test',
+<% }
+if (yarn) { %>    useYarn: true,
 <% } %>    scenarios: [
       {
         name: 'ember-lts-3.12',
